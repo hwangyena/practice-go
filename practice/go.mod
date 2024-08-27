8c27fdf78b61fa875eac5b7d69149352e3fb294c
SK플래닛 황예나 <center7332@naver.com>
2024-08-27 09:44:54 +0900

    feat: gpt 연결 코드 추가

--- practice/go.mod
+++ practice/go.mod
@@ -1,3 +1,8 @@
 module practice
 
 go 1.22.6
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sashabaranov/go-openai v1.28.2 // indirect
+)
--- practice/practice.go
+++ practice/practice.go
@@ -1,11 +1,44 @@
 package main
 
-func main(){
-	// fmt.Println(Calc([1]))
-	arr := []int {34, 67, 55, 33, 12, 98}
-	Calc(arr)
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	
+	if err != nil {
+		log.Fatal("ERROR!")
+	}
+	
+	config := openai.DefaultConfig("bedrock")
+	config.BaseURL = os.Getenv("BEDROCK_URL") // AWS Bedrock
+
+	client := openai.NewClientWithConfig(config)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo0125,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
 }
 
-func Calc(n int) {
-	
-}
